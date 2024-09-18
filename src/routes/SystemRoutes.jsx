c0199cf8ea2ca5d9a9bca797e423727a0359ce76
zeketo <ikorofrancis24@gmail.com>
2024-09-18 16:51:02 +0100

    styles folder

--- src/routes/SystemRoutes.jsx
+++ src/routes/SystemRoutes.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '../pages/home/Home'
+import About from '../pages/about/About'
+import Contact from '../pages/contactUs/Contact'
 
 const SystemRoutes = () => {
   return (
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/contact' element={<Contact/>}/>
         </Routes>
     </BrowserRouter>
   )
